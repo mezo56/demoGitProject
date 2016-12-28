ef9355eabd125f4589fac9f5745ddc10e492865b
lli-9 <lli-9@10.131.219.41>
2016-12-28 13:54:17 +0300

    i added varibale z

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -5,11 +5,13 @@ public class firstclass {
 	public static void main(String[] args) {
 		int x=5;
 		int y=7;
+		int z = 9 ;
 		System.out.println("please add two numbers");
 		System.out.println("Result is: "+ add(x,y));
 		//add(x,y);
 
 	}
+	
 	
 	public static int add(int x, int y){
 		int result=x+y;
